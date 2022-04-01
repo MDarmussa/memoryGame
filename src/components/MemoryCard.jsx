844ea7025bce248ab2a6d16526064afca093a3ca
Mohammad <mhamed.mussa@gmail.com>
2022-03-31 21:08:34 -0500

    first commit

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './MemoryCard.css'
+
+class MemoryCard extends React.Component {
+     render() {
+          return(
+                <div className="MemoryCard">
+                     <img className="image" src="./digitalcrafts-logo-white-y.png" alt="DC-Logo" />
+
+                </div>
+              
+          )
+     }
+
+}
+
+export default MemoryCard;
+
+
