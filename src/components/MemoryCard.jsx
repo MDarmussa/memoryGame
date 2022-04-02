4ca861bf4a5eb307377a51ab5cf6592eef07b879
Mohammad <mhamed.mussa@gmail.com>
2022-04-02 13:36:04 -0500

    Game Completed

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import './MemoryCard.css'
 
 class MemoryCard extends React.Component {
+
      render() {
           return(
-                <div className="MemoryCard">
-                     <img className="image" src="./digitalcrafts-logo-white-y.png" alt="DC-Logo" />
-
-                </div>
-              
+               <>
+               <div className="MemoryCard" onClick={this.props.pickCard}>
+                    <div className= {!this.props.isFlipped ? 'MemoryCardInner' : 'MemoryCardInner flipped'}>
+                         <div className="MemoryCardBack">
+                              <img className="image" src="./digitalcrafts-logo-white-y.png" alt="DC-Logo" />
+                          </div>
+                         <div className="MemoryCardFront">
+                              {this.props.symbol}
+                         </div> 
+                    </div>
+               </div>
+               </>            
           )
      }
 
